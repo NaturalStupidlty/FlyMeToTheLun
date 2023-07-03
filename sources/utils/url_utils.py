fe0e82e4badc0f1a855c84e2d098e59f78611cec
RaynGoslenko <olkhovatyi@knu.ua>
2023-07-03 23:52:11 +0300

    Updated converting urls to filenames

--- sources/utils/url_utils.py
+++ sources/utils/url_utils.py
@@ -1,5 +1,6 @@
 import re
 import os
+import pandas as pd
 
 
 FILENAME_PATTERN = re.compile(r"(\d{9}.\w+)")
@@ -11,3 +12,10 @@ def get_image_path_from_url(url: str):
     image_path = os.path.join("..", "..", "data", "images", filename)
 
     return image_path
+
+
+def create_filenames_csv(dataframe: pd.DataFrame) -> pd.DataFrame:
+    dataframe["image_url1"] = dataframe["image_url1"].apply(lambda x: FILENAME_PATTERN.search(x).group(1))
+    dataframe["image_url2"] = dataframe["image_url2"].apply(lambda x: FILENAME_PATTERN.search(x).group(1))
+
+    return dataframe
